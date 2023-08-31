830004a93512872b7254e1f5f26edccc6418902a
Marko Grdinić <mrakgr@gmail.com>
2023-08-31 16:50:31 +0200

    Ok, I have the parser. How do I put errors into the editor? Where is the diagnostic collection? Where are the semantic token providers?

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
+/* eslint-disable no-unexpected-multiline */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { MutableRefObject, useRef, useState } from 'react'
 import './App.css'
 
 import Editor, {Monaco} from '@monaco-editor/react'
 import { editor } from "monaco-editor"
+
+import {string, whitespace, float, Parjser, ParjsCombinator} from 'parjs'
+import {between, or, then, qthen, thenq, map, mapConst, many} from 'parjs/combinators'
+
+type Op =
+  | [op: "center"]
+  | [op: "penup"]
+  | [op: "pendown"]
+  | [op: "forward", length: number]
+  | [op: "backward", length: number]
+  | [op: "turnleft", degree: number]
+  | [op: "turnright", degree: number]
+  | [op: "direction", degree: number]
+  | [op: "gox", x: number]
+  | [op: "goy", y: number]
+  | [op: "penwidth", width: number]
+  | [op: "go", data: {x: number, y: number}]
+  | [op: "pencolor", data: {x : number, y : number,z : number}]
+
+  
+const pFloat = () => float().pipe(thenq(whitespace()))
+const pComma = () => string(",").pipe(thenq(whitespace()))
+
+const pTemplate = <T extends string>(key : T) => whitespace().pipe(qthen(key)).pipe((thenq(whitespace()))).pipe(mapConst(key))
+const pZero = <T extends string>(key : T) => pTemplate(key).pipe(map((x) : [T] => [x]))
+const pCenter = () => pZero("center")
+const pPenUp = () => pZero("penup")
+const pPenDown = () => pZero("pendown")
+
+const pOne = <T extends string>(key : T) => pTemplate(key).pipe(then(pFloat()))
+const pForward = () => pOne("forward")
+const pBackward = () => pOne("backward")
+const pLeft = () => pOne("turnleft")
+const pRight = () => pOne("turnright")
+const pDirection = () => pOne("direction")
+const pGoX = () => pOne("gox")
+const pGoY = () => pOne("goy")
+const pPenWidth = () => pOne("penwidth")
+
+const pFloatComma = () => pFloat().pipe(thenq(pComma()))
+const pXY = () => pFloatComma().pipe(then(pFloat())).pipe(map(([x,y]) => ({x,y})))
+const pXYZ = () => pFloatComma().pipe(then(pFloatComma(), pFloat())).pipe(map(([x,y,z]) => ({x,y,z})))
+const pGo = () => pTemplate("go").pipe(then(pXY()))
+const pPenColor = () => pTemplate("pencolor").pipe(then(pXYZ()))
+
+const pStatement = () : Parjser<Op> =>
+  pCenter().pipe(or(pPenUp(), pPenDown())) // 0
+    .pipe(or(pForward(),pBackward(),pLeft(),pRight())) // 1
+    .pipe(or(pDirection(), pGoX(), pGoY(), pPenWidth())) // 1
+    .pipe(or(pGo())) // XY
+    .pipe(or(pPenColor())) // XYZ
+
+const pLogo = () : Parjser<Op[]> =>
+  pStatement().pipe(many())
 
 function App() {
   const editorRef : MutableRefObject<editor.IStandaloneCodeEditor | null> = useRef(null);
@@ -14,12 +69,19 @@ function App() {
   }
 
   function showValue() {
-    alert(editorRef.current?.getValue());
+    const editor = editorRef.current
+    if (!editor) throw Error("Editor not defined.")
+    
+    // const text = editor.getValue()
+    // alert(pLogo().parse(text));
   }
 
-  function drawSomething(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-    const ctx = canvasRef.current?.getContext("2d")
-    if (!ctx) throw Error("Context not defined.")
+  function drawSomething(): void {
+    const canvas = canvasRef.current
+    if (!canvas) throw Error("Canvas not defined.")
+    const ctx = canvas.getContext("2d")
+    if (!ctx) throw Error("Canvas is null.")
+
     ctx.fillRect(50, 50, 100, 100);
   }
 
@@ -28,8 +90,9 @@ function App() {
       <div style={{flex: 1}}>
         <button onClick={showValue}>Show value</button>
         <Editor 
-          defaultLanguage="javascript" 
-          defaultValue="// some comment" 
+          defaultLanguage="plaintext"
+          defaultValue="forward 150
+backward"
           onMount={handleEditorDidMount}
           />
       </div>
