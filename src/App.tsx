67aadf1e3653aa4fc15c411925435692b5d8a10f
Marko Grdinić <mrakgr@gmail.com>
2023-08-31 19:39:05 +0200

    parjs handles errors in the choice combinators poorly. I can either switch to some other library or try to adjust the error message myself.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@
 import { MutableRefObject, useRef, useState } from 'react'
 import './App.css'
 
-import Editor, {Monaco} from '@monaco-editor/react'
-import { editor, MarkerSeverity } from "monaco-editor"
+import * as monaco from "monaco-editor"
 
-import {string, whitespace, float, Parjser, ParjsCombinator} from 'parjs'
-import {between, or, then, qthen, thenq, map, mapConst, many} from 'parjs/combinators'
+import { string, whitespace, float, Parjser, ParjsCombinator } from 'parjs'
+import { between, or, then, qthen, thenq, map, mapConst, many } from 'parjs/combinators'
 
 type Op =
   | [op: "center"]
@@ -21,20 +20,20 @@ type Op =
   | [op: "gox", x: number]
   | [op: "goy", y: number]
   | [op: "penwidth", width: number]
-  | [op: "go", data: {x: number, y: number}]
-  | [op: "pencolor", data: {x : number, y : number,z : number}]
+  | [op: "go", data: { x: number, y: number }]
+  | [op: "pencolor", data: { x: number, y: number, z: number }]
 
 
 const pFloat = () => float().pipe(thenq(whitespace()))
 const pComma = () => string(",").pipe(thenq(whitespace()))
 
-const pTemplate = <T extends string>(key : T) => whitespace().pipe(qthen(key)).pipe((thenq(whitespace()))).pipe(mapConst(key))
-const pZero = <T extends string>(key : T) => pTemplate(key).pipe(map((x) : [T] => [x]))
+const pTemplate = <T extends string>(key: T) => whitespace().pipe(qthen(key)).pipe((thenq(whitespace()))).pipe(mapConst(key))
+const pZero = <T extends string>(key: T) => pTemplate(key).pipe(map((x): [T] => [x]))
 const pCenter = () => pZero("center")
 const pPenUp = () => pZero("penup")
 const pPenDown = () => pZero("pendown")
 
-const pOne = <T extends string>(key : T) => pTemplate(key).pipe(then(pFloat()))
+const pOne = <T extends string>(key: T) => pTemplate(key).pipe(then(pFloat()))
 const pForward = () => pOne("forward")
 const pBackward = () => pOne("backward")
 const pLeft = () => pOne("turnleft")
@@ -45,50 +44,72 @@ const pGoY = () => pOne("goy")
 const pPenWidth = () => pOne("penwidth")
 
 const pFloatComma = () => pFloat().pipe(thenq(pComma()))
-const pXY = () => pFloatComma().pipe(then(pFloat())).pipe(map(([x,y]) => ({x,y})))
-const pXYZ = () => pFloatComma().pipe(then(pFloatComma(), pFloat())).pipe(map(([x,y,z]) => ({x,y,z})))
+const pXY = () => pFloatComma().pipe(then(pFloat())).pipe(map(([x, y]) => ({ x, y })))
+const pXYZ = () => pFloatComma().pipe(then(pFloatComma(), pFloat())).pipe(map(([x, y, z]) => ({ x, y, z })))
 const pGo = () => pTemplate("go").pipe(then(pXY()))
 const pPenColor = () => pTemplate("pencolor").pipe(then(pXYZ()))
 
-const pStatement = () : Parjser<Op> =>
+const pStatement = (): Parjser<Op> =>
   pCenter().pipe(or(pPenUp(), pPenDown())) // 0
-    .pipe(or(pForward(),pBackward(),pLeft(),pRight())) // 1
+    .pipe(or(pForward(), pBackward(), pLeft(), pRight())) // 1
     .pipe(or(pDirection(), pGoX(), pGoY(), pPenWidth())) // 1
     .pipe(or(pGo())) // XY
     .pipe(or(pPenColor())) // XYZ
 
-const pLogo = () : Parjser<Op[]> =>
-  pStatement().pipe(many())
+// const pLogo = () : Parjser<Op[]> =>
+//   pStatement().pipe(many())
 
 function App() {
-  const editorRef : MutableRefObject<editor.IStandaloneCodeEditor | null> = useRef(null);
-  const canvasRef : MutableRefObject<HTMLCanvasElement | null> = useRef(null);
+  const editorRef: MutableRefObject<monaco.editor.IStandaloneCodeEditor | null> = useRef(null);
+  const canvasRef: MutableRefObject<HTMLCanvasElement | null> = useRef(null);
 
-  function handleEditorDidMount(editor: editor.IStandaloneCodeEditor, monaco: Monaco) {
-    editorRef.current = editor;
+  function setupMonaco(el: HTMLDivElement) {
+    if (!editorRef.current) {
+      const uri = monaco.Uri.parse("inmemory://test");
+      const value = "forward 150\nbackward"
+      const model = monaco.editor.createModel(value, "logo-clone-lang", uri);
+      const editor = monaco.editor.create(el, { model });
+      validate(model);
+      model.onDidChangeContent(() => {
+        validate(model);
+      });
+      editorRef.current = editor
+    }
+  }
+
+  function validate(model: monaco.editor.ITextModel) {
+    const statements = model.getLinesContent().map((x, i) => ({ line: i + 1, statement: pStatement().parse(x) }))
+    const errors: monaco.editor.IMarkerData[] = []
+    const ops: Op[] = []
+    for (const st of statements) {
+      switch (st.statement.kind) {
+        case 'OK': {
+          ops.push(st.statement.value)
+          break
+        }
+        default: {
+          errors.push({
+            severity: monaco.MarkerSeverity.Error,
+            message: st.statement.reason.toString(),
+            startLineNumber: st.line,
+            endLineNumber: st.line,
+            startColumn: st.statement.trace.location.column,
+            endColumn: model.getLineLength(st.line)+1,
+          })
+          break
+        }
+      }
+    }
+    monaco.editor.setModelMarkers(model, '', errors)
   }
 
   function showValue() {
     const x = editorRef.current
     if (!x) throw Error("Editor not defined.")
-    
+
     const model = x.getModel()
     if (!model) throw Error("Model not defined.")
-    const m : editor.IMarkerData = {
-      severity: MarkerSeverity.Error,
-      message: "qwe",
-      startLineNumber: 1,
-      endLineNumber: 2,
-      startColumn: 1,
-      endColumn: 1,
-    }
-    
-    const old = editor.getModelMarkers({})
-    alert(JSON.stringify(old))
-    editor.setModelMarkers(model,'',[m])
-    const text = model.getValue()
-    // alert(text)
-    // alert(pLogo().parse(text));
+    validate(model)
   }
 
   function drawSomething(): void {
@@ -101,19 +122,15 @@ function App() {
   }
 
   return (
-    <div style={{width: "1800px", height: "900px", backgroundColor: "lightgray", border: "2px gray solid", display: "flex", flexDirection: "row"}}>
-      <div style={{flex: 1}}>
+    <div style={{ width: "1800px", height: "900px", backgroundColor: "lightgray", border: "2px gray solid", display: "flex", flexDirection: "row" }}>
+      <div style={{ flex: 1 }}>
         <button onClick={showValue}>Show value</button>
-        <Editor 
-          defaultValue="forward 150
-backward"
-          onMount={handleEditorDidMount}
-          />
+        <div ref={setupMonaco} style={{ height: "100%" }}></div>
       </div>
-      <div style={{flex: 1}}>
+      <div style={{ flex: 1 }}>
         <button onClick={drawSomething}>Draw Something</button>
         <div>
-          <canvas ref={canvasRef}>Your browser does not support the canvas element.</canvas>
+          <canvas ref={canvasRef} >Your browser does not support the canvas element.</canvas>
         </div>
       </div>
     </div>
